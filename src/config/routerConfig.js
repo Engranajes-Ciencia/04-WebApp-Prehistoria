e40b8f51525a82f0db220992a17c86972ec5e145
VictorMGmedac <vmg0009@alu.medac.es>
2025-04-07 13:07:28 +0200

    Carpeta config con su routerConfig
    App.jsx más limpio al llamar desde AppRouter.jsx las rutas de las páginas

---
+++ src/config/routerConfig.js
@@ -0,0 +1,15 @@
+
+
+const ROUTES = {
+    MODOJUEGO: '/',
+    PORTADA: '/portada',
+    FORM: '/form',
+    MAPA: '/mapa',
+    ENTREACTIVIDADES: '/entre-actividad',
+    ESCANERQR: '/escanerQR',
+    ACTIVIDAD: '/actividad/:id',
+    FINAL: '/final',
+    VITRINA: '/vitrina',
+};
+
+export default ROUTES;
