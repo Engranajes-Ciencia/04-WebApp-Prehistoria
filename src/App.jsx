2777291f4c688133676a68a88b6fdaa79b1e2546
MFlorenciaPC <mpc0043@alu.medac.es>
2025-04-24 19:58:40 +0200

    añadido el modo landscape
    muestra un mensaje si se gira la pantalla a modo vertical (la idea es que en la tablet sea al reves pero para trabajar en el ordenador es imposible con la pantalla en vertical)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { UseOrientation } from "./UseOrientation";
 import AppRouter from "./config/routes/AppRouter";
 import "./index.css";
 
 function App() {
+  const orientation = UseOrientation(); //Detectamos la orientación
   return (
-      <AppRouter />
+    <div>
+      {/*Mostrar mensaje solo en modo Landscape*/}
+      {orientation === "portrait" && (
+        <div className="orientacion-alerta">
+          Por favor, gira tu dispositivo a modo vertical para ver el contenido
+        </div>
+      )}
+      {orientation === "landscape" && <AppRouter />}
+    </div>
   );
 }
 
-
 export default App;
-
