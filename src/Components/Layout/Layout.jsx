498ee63084142c9fa1f1e8fe7b25456a3009e159
JesusValhermoso <jvc0012@alu.medac.es>
2025-04-25 12:48:42 +0200

    Se añade temporizador de inactividad

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,13 +2,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "../../Styles/Layout/Layout.css";
 import ConnectionAlert from "../Commons/ConnectionAlert";
+import InactivityTimer from "../Commons/InactivityTimer";
 
 const Layout = ({ children }) => {
 
     const isDark = document.documentElement.classList.contains('dark');
 
+    const handleInactivity = () => {
+        alert("Inactividad detectada. Cerrando sesión...");
+    };
+
     return (
         <div className={`layout ${isDark ? 'modo-oscuro' : ''}`}>
+            <InactivityTimer timeout={300000} onTimeout={handleInactivity} />
             <Header />
             <main>{children}</main>
             <ConnectionAlert />
