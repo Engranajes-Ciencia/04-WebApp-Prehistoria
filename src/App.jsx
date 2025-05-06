1219d57182c0e8f57cddfcb94d3ff86b79913c62
MFlorenciaPC <mpc0043@alu.medac.es>
2025-05-06 18:04:38 +0200

    Preloader
    agrego un preloader solo al cargar la primera página, con un spinner básico y el texto "Cargando" con animación de 3 puntos suspensivos. Dura 1 segundo (se puede aumentar o disminuir)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { UseOrientation } from "./UseOrientation";
+import "./App.css";
 import AppRouter from "./config/routes/AppRouter";
+import Layout from "./components/Layout/Layout";
 import "./index.css";
-import Layout from "./Components/Layout/Layout";
 import "./Styles/Commons/OrientationWarning.css";
 
+
 function App() {
-  const orientation = UseOrientation(); //Detecta la orientación
+  const orientation = UseOrientation(); // Detecta orientación
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Solo la primera vez que se carga la app
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // 1 segundo de preloader
+
+    return () => clearTimeout(timer);
+  }, []); // se ejecuta solo al montar
+
+  if (loading) {
+    return (
+      <div className="preloader">
+        <div className="spinner"> </div>
+        <p className="cargando" >Cargando <span className="dots"
+        ></span> </p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      {/*Mostrar alerta solo en modo vertical*/}
       {orientation === "portrait" && (
         <div className="orientacion-alerta">
-         ⚠️ Para una mejor experiencia recomendamos utilizar el dispositivo en
-          modo horizontal{" "}
+          ⚠️ Para una mejor experiencia recomendamos utilizar el dispositivo en modo horizontal
         </div>
       )}
       <Layout>
@@ -24,3 +45,5 @@ function App() {
 }
 
 export default App;
+
+
