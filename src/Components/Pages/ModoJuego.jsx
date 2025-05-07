3b348a6699ca3cd343e799a58580650520d08c1f
MFlorenciaPC <mpc0043@alu.medac.es>
2025-05-07 13:45:20 +0200

    cambios en pagina de inicio
    modificacion de imagenes solo y grupo, cambio "solitario" por individual", ubicacion mas baja de los textos modo invididual y grupo, quite la opacidad en el hover y agregue filtro iluminado en modo individual. Faltan cambios en el modo grupo

--- src/Components/Pages/ModoJuego.jsx
+++ src/Components/Pages/ModoJuego.jsx
@@ -2,43 +2,50 @@ import { useNavigate } from "react-router-dom";
 import "../../Styles/Pages/ModoJuego.css";
 
 function ModoJuego() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleChoice = (choice) => {
-        if (choice === "solo") {
-            navigate("/portada");
-        } else if (choice === "grupos") {
-            navigate("/portada");
-        }
-    };
+  const handleChoice = (choice) => {
+    if (choice === "solo") {
+      navigate("/portada");
+    } else if (choice === "grupos") {
+      navigate("/portada");
+    }
+  };
 
-    return (
-        <div className="modojuego-container">
-            <h1 className="titulo">Elige tu estilo de juego</h1>
-            <p className="subtitulo">¿Exploras solo o acompañado?</p>
-            <div className="opciones">
-                <button
-                    className="btn-opcion" id="solo"
-                    onClick={() => handleChoice("solo")}
-                    style={{
-                        backgroundImage: `url(${import.meta.env.BASE_URL}assets/images/solo.jpg)`
-                    }}
-                >
-                    <span>Modo solitario</span>
-                </button>
-                <button
-                    className="btn-opcion" id="grupo"
-                    onClick={() => handleChoice("grupos")}
-                    style={{
-                        backgroundImage: `url(${import.meta.env.BASE_URL}assets/images/grupo.jpg)`
-                    }}
-
-                >
-                    <span>Modo equipo</span>
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="modojuego-container">
+      <h1 className="titulo">Elige tu estilo de juego</h1>
+      <p className="subtitulo">¿Exploras solo o acompañado?</p>
+      <div className="opciones">
+        <button
+          className="btn-opcion"
+          id="solo"
+          onClick={() => handleChoice("solo")}
+          style={{
+            backgroundImage: `url(${
+              import.meta.env.BASE_URL
+            }assets/images/solo_3d.png)`,
+          }}
+        >
+          <span>Modo individual</span>
+        </button>
+       
+        <button
+          className="btn-opcion" 
+          id="grupo" 
+          onClick={() => handleChoice("grupos")}
+          style={{
+            backgroundImage: `url(${
+              import.meta.env.BASE_URL
+            }assets/images/grupo_3d.png)`,
+          }} 
+        >
+          <span>Modo equipo</span>
+          
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ModoJuego;
