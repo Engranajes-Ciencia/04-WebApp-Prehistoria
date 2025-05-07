9449627ade90439f8dc3317cdc03aad6a0c0c6ba
Iancamps <icg0012@alu.medac.es>
2025-05-07 13:11:43 +0200

    cambio imagenes fondo y avatar a la pagina de form
    se generan nuevas imagenes de los fondos del form y las imagenes de los avatares mas se cambia estilo css

--- src/Components/Pages/Form.jsx
+++ src/Components/Pages/Form.jsx
@@ -26,12 +26,6 @@ function Form() {
     `${import.meta.env.BASE_URL}assets/form-fondo/fondo4.png`,
     `${import.meta.env.BASE_URL}assets/form-fondo/fondo5.png`,
     `${import.meta.env.BASE_URL}assets/form-fondo/fondo6.png`,
-   /* `${import.meta.env.BASE_URL}assets/form-fondo/foto1.png`,
-    `${import.meta.env.BASE_URL}assets/form-fondo/foto2.jpg`,
-    `${import.meta.env.BASE_URL}assets/form-fondo/foto3.jpg`,
-    `${import.meta.env.BASE_URL}assets/form-fondo/foto4.jpg`,
-    `${import.meta.env.BASE_URL}assets/form-fondo/foto5.png`,
-    `${import.meta.env.BASE_URL}assets/form-fondo/foto6.png`,*/
   ];
 
   //  Cambio automático del fondo cada 5 segundos
