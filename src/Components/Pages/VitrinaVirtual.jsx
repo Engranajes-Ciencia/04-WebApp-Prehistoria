437c0808520dd2fb61ce1e9ff7f27a82f5ae02d6
MFlorenciaPC <mpc0043@alu.medac.es>
2025-05-19 13:32:33 +0200

    cambios en página Vitrina Virtual
    reorganización de las medallas por orden de las paradas. Quité las medallas que ya no sirven pero no las eliminé, siguen el la carpeta de imágenes

--- src/Components/Pages/VitrinaVirtual.jsx
+++ src/Components/Pages/VitrinaVirtual.jsx
@@ -5,16 +5,16 @@ import "../../Styles/Pages/VitrinaVirtual.css";
 
 
 const medallas = [
-    { id: 1, titulo: "Fósiles prehistóricos", imagen: "medalla1.png", curiosidad: "Los trilobites existieron antes que los dinosaurios." },
-    { id: 2, titulo: "Dinosaurios gigantes", imagen: "medalla2.png", curiosidad: "El velociraptor tenía plumas." },
-    { id: 3, titulo: "Tortuga de las Galápagos", imagen: "medalla3.png", curiosidad: "Su caparazón puede cambiar según la isla." },
-    { id: 4, titulo: "Huellas de Laetoli", imagen: "medalla4.png", curiosidad: "Pertenecen a Australopithecus afarensis." },
-    { id: 5, titulo: "Pinturas rupestres", imagen: "medalla5.png", curiosidad: "Algunas pinturas tienen más de 30.000 años." },
-    { id: 6, titulo: "Poblado nómada", imagen: "medalla6.png", curiosidad: "Vivían en chozas y seguían a los animales." },
+    { id: 1, titulo: "Primeras plantas", imagen: "", curiosidad: "Los trilobites existieron antes que los dinosaurios." },
+    { id: 2, titulo: "Primeras flores", imagen: "", curiosidad: "El velociraptor tenía plumas." },
+    { id: 3, titulo: "Segundos dinosaurios", imagen: "medalla2.png", curiosidad: "Su caparazón puede cambiar según la isla." },
+    { id: 4, titulo: "Tortugas de las Galápagos", imagen: "medalla3.png", curiosidad: "Pertenecen a Australopithecus afarensis." },
+    { id: 5, titulo: "Atapuerca", imagen: "", curiosidad: "Algunas pinturas tienen más de 30.000 años." },
+    { id: 6, titulo: "Pinturas rupestres", imagen: "medalla5.png", curiosidad: "Vivían en chozas y seguían a los animales." },
     { id: 7, titulo: "Poblado sedentario", imagen: "medalla7.png", curiosidad: "Comenzaron a construir aldeas permanentes." },
-    { id: 8, titulo: "Pozo", imagen: "medalla8.png", curiosidad: "Los primeros pozos datan del Neolítico." },
-    { id: 9, titulo: "Stonehenge", imagen: "medalla9.png", curiosidad: "Algunas alineaciones indican los solsticios." },
-    { id: 10, titulo: "Templo de Karnak", imagen: "medalla10.png", curiosidad: "Cada faraón dejaba su huella en Karnak." }
+    { id: 8, titulo: "Poblados nómadas", imagen: "medalla6.png", curiosidad: "Los primeros pozos datan del Neolítico." },
+    { id: 9, titulo: "Çatalhöyük", imagen: "", curiosidad: "Algunas alineaciones indican los solsticios." },
+    { id: 10, titulo: "Stonehenge", imagen: "medalla9.png", curiosidad: "Cada faraón dejaba su huella en Karnak." }
 ];
 
 
@@ -25,7 +25,7 @@ function VitrinaVirtual() {
 
     return (
         <div className="vitrina-virtual-container">
-            <h1 className="titulo-virtual">Galería Virtual Medallas Prehistóricas</h1>
+            <h1 className="titulo-virtual">"Galería Virtual Medallas Prehistóricas"</h1>
             <p className="contador-medallas">Has conseguido {completadas.length} de {medallas.length} medallas</p>
 
             <div className="grid-medallas">
