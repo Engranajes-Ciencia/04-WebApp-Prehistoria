0db9b6331158b4f59af9bc131583ba0baed1147e
Iancamps <icg0012@alu.medac.es>
2025-05-20 13:11:40 +0200

    cambio rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { UseOrientation } from "./UseOrientation";
 import "./App.css";
-import AppRouter from "./config/routes/AppRouter";
 import Layout from "./components/Layout/Layout";
 import "./index.css";
 import "./Styles/Commons/OrientationWarning.css";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+import AppRouter from "./config/routes/AppRouter";
 
 
 function App() {
@@ -30,17 +33,25 @@ function App() {
     );
   }
 
+  // Define la base URL dinámicamente.
+  // import.meta.env.VITE_BASE_URL se lee de tus archivos .env
+  const BASE_URL = import.meta.env.VITE_BASE_URL || '/';
+
   return (
-    <div>
-      {orientation === "portrait" && (
-        <div className="orientacion-alerta">
-          ⚠️ Para una mejor experiencia recomendamos utilizar el dispositivo en modo horizontal
-        </div>
-      )}
-      <Layout>
-        <AppRouter />
-      </Layout>
-    </div>
+    <Router basename={BASE_URL}>
+      <div>
+        {orientation === "portrait" && (
+          // Considerar añadir un botón para cerrar esta alerta si es persistente
+          <div className="orientacion-alerta">
+            ⚠️ Para una mejor experiencia recomendamos utilizar el dispositivo
+            en modo horizontal
+          </div>
+        )}
+        <Layout>
+          <AppRouter />  {/* AppRouter contiene todas tus <Routes> y <Route> */}
+        </Layout>
+      </div>
+    </Router>
   );
 }
 
--- src/config/routes/AppRouter.jsx
+++ src/config/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ROUTES from './routerConfig';
 import { lazy, Suspense } from "react";
 
